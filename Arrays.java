d9106f375b99540a3a436755ed1beed0a8d7ad2d
oscar2020 <41194339+oscar2020@users.noreply.github.com>
2019-01-19 18:37:59 +0000

    upload files

---
+++ Arrays.java
@@ -0,0 +1,20 @@
+package classPerson;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+       int table[][] = new int [3][4];
+        for (int i=0;i<3;i++) {
+        	
+        	for (int j=0;j<4;j++) {
+        		
+        		System.out.println(table[i][j]);
+        	}
+        	
+        }
+		
+	}
+
+}
